90d1587a1ee120e1171184e806db5d37ce4303bb
Eastern <easternlai@hotmail.com>
2024-01-29 01:47:54 -0800

    worked on layout of tech page

--- app/(portfolio)/tech/page.tsx
+++ app/(portfolio)/tech/page.tsx
@@ -1,6 +1,6 @@
 const TechPage = () => {
     return (
-        <div>Tech</div>
+        <div className="text-2xl font-light tracking-wider leading-10">This portfolio was built on a kubernetes cluster in AWS.</div>
     )
 }
 
