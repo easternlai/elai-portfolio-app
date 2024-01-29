075663405114e590abb153c46fb94d8156931505
Eastern <easternlai@hotmail.com>
2024-01-28 21:08:45 -0800

    added formating to heading

--- app/(portfolio)/_components/header.tsx
+++ app/(portfolio)/_components/header.tsx
@@ -1,22 +1,33 @@
 import Image from "next/image"
-import Link from "next/link"
 import NavLink from "./navlink"
-
+import { Rubik } from "next/font/google"
+import { cn } from "@/lib/utils"
+const headingFont = Rubik({
+    subsets: ["latin"],
+    weight: [
+        "300",
+        "400",
+        "500",
+        "600",
+        "700",
+        "800"
+    ]
+})
 export const Header = () => {
     return (
-        <div className="flex space-x-14">
+        <div className="flex space-x-14 mb-12">
             <div>
                 <Image className="rounded-full" src="/profile.png" width={150} height={100} alt="Organization" />
             </div>
 
-            <div className="flex-col">
+            <div className={cn(headingFont.className, "flex-col")}>
                 <div className="flex">
-                    <NavLink href="/" className="pb-5 border-b-8 border-[#4B6CC1] text-4xl font-bold mt-5">EASTERN LAI</NavLink>
+                    <NavLink href="/" className="pb-3 border-b-8 border-[#4B6CC1] text-4xl font-bold mt-4">EASTERN LAI</NavLink>
                 </div>
-                <nav className="flex space-x-10 mt-12 text-3xl">
-                    <NavLink href="/career" className="">career</NavLink>
-                    <NavLink href="/tech" className="">tech stack </NavLink>
-                    <NavLink href="/about" className="">about me</NavLink>
+                <nav className="flex space-x-10 mt-8 text-3xl">
+                    <NavLink href="/career" className="">Career</NavLink>
+                    <NavLink href="/tech" className="">Tech Stack </NavLink>
+                    <NavLink href="/about" className="">About Me</NavLink>
                 </nav>
             </div>
         </div>
--- app/(portfolio)/layout.tsx
+++ app/(portfolio)/layout.tsx
@@ -1,9 +1,24 @@
+import { cn } from "@/lib/utils"
 import { Header } from "./_components/header"
+import { Karla } from "next/font/google"
+
+const mainFont = Karla({
+    subsets: ["latin"],
+    weight: [
+        "200",
+        "300",
+        "400",
+        "500",
+        "600",
+        "700",
+        "800"
+    ]
+})
 
 const PortfolioLayout = ({ children }: { children: any }) => {
 
     return (
-        <div className="mx-96 px-12 pt-12">
+        <div className={cn(mainFont.className, "mx-96 px-12 pt-12")}>
             <Header />
             <div>
                 {children}
