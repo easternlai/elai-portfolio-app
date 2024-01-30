66af6e5f0072d9a9ba41b7229b9d16fd106a1752
Eastern <easternlai@hotmail.com>
2024-01-29 22:54:21 -0800

    resolved sizing issue with navbar

--- app/(portfolio)/about/page.tsx
+++ app/(portfolio)/about/page.tsx
@@ -1,9 +1,11 @@
 const AboutPage = () => {
     return (
-        <div className="flex-col space-y-1">
-            <div className="text-lg"><span className="font-bold">contact:</span> <span className="italic">eastern.lai@gmail.com</span> | 415.420.1297</div>
-            <div className="text-lg"><span className="font-bold">github: </span><a className="underline" href="https://github.com/easternlai">https://github.com/easternlai</a></div>
-            <div className="text-lg"><span className="font-bold">LinkedIn: </span><a className="underline" href="https://www.linkedin.com/in/eastern-lai-3b05b585/">https://www.linkedin.com/in/eastern-lai-3b05b585/</a></div>
+        <div>
+            <div className="flex-col space-y-3">
+                <div className="text-2xl"><span className="font-bold">contact:</span> <span className="italic">eastern.lai@gmail.com</span> | 415.420.1297</div>
+                <div className="text-2xl"><span className="font-bold">github: </span><a className="underline" href="https://github.com/easternlai">https://github.com/easternlai</a></div>
+                <div className="text-2xl"><span className="font-bold">LinkedIn: </span><a className="underline" href="https://www.linkedin.com/in/eastern-lai-3b05b585/">https://www.linkedin.com/in/eastern-lai-3b05b585/</a></div>
+            </div>
         </div>
     )
 }
